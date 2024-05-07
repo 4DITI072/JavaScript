55e01f08b77d8dd740d198e22f4f75d0ce91cad9
4DITI072 <119050868+4DITI072@users.noreply.github.com>
2024-05-07 09:24:30 +0000

    added new files

---
+++ basics/datatypes.js
@@ -0,0 +1,38 @@
+//Primitives : 
+/*
+Strings
+numbers
+undefined
+null - standalone value
+boolean
+symbol
+object
+*/ 
+let name = "Aditi"
+let age = 22
+let score = 9.9
+let state 
+let isloggedin = true
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof score);
+console.log(typeof state);
+console.log(typeof null);
+console.log(typeof isloggedin);
+
+//Objects in JS -
+
+//Datatype Conversion in JS
+let scor = "93"
+let scoreinnum = Number(scor)
+console.log(typeof(scoreinnum));
+console.log(typeof(scor));
+
+let val = "93abc"
+let valinnum = Number(val)
+console.log(typeof(valinnum));
+console.log(typeof(val));
+console.log(valinnum); // GIVES NaN - not a number
+
+ 
---
+++ basics/variables.js
@@ -0,0 +1,13 @@
+const accID = 143
+let email = "aditi@gmail.com"
+var pass = "12345"
+accCity = "Bhopal"
+
+//accID = 123 - COMMENT As Not Allowed
+
+email = "hcb@gmail.com"
+pass = "22222"
+accCity = "Banglore"
+
+console.table([accID,accCity,pass,email]);
+console.log(accID);
