1ecfa21b8faee504bc7d73e4bf64822b54602064
Aditi Kaushal <119050868+4DITI072@users.noreply.github.com>
2025-02-12 22:41:40 +0530

    Update variables.js

--- basics/variables.js
+++ basics/variables.js
@@ -11,3 +11,6 @@ accCity = "Banglore"
 
 console.table([accID,accCity,pass,email]);
 console.log(accID);
+
+// basically let and var are different due to scope issues : use only let and const and [not var - as it does not have a scope]
+//also js is flexible enough to allow variable creation without any datatype but dont do it. 
