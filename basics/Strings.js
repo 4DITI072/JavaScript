f4d72ad78d0902bac43266b0ca2028d5845b72be
Aditi Kaushal <119050868+4DITI072@users.noreply.github.com>
2025-02-12 23:56:57 +0530

    Update Strings.js

--- basics/Strings.js
+++ basics/Strings.js
@@ -1,9 +1,9 @@
 let name = "Aditi"
 let repocount = 21
 
-console.log(`My name is ${name} and my repo count is ${repocount}`);
+console.log(`My name is ${name} and my repo count is ${repocount}`); //interpolation : place holders 
 //console.log(`My name is ${name.ToUpperCase} and my repo count is ${repocount}`);
 
 const gameName = new String('Lucifer')
-console.log(gameName[0]);
-console.log(gameName.__proto__);
+console.log(gameName[0]); // first letter 
+console.log(gameName.__proto__); // -- >  {}
