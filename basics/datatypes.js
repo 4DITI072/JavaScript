9f9efd6371b7aef7a9d22050fef6e2322b4a60dd
Aditi Kaushal <119050868+4DITI072@users.noreply.github.com>
2025-02-12 23:32:34 +0530

    Update datatypes.js

--- basics/datatypes.js
+++ basics/datatypes.js
@@ -49,7 +49,11 @@ console.log("2" + 2);
 console.log(1+2+"2");
 //output would be 32 of type string  BUT
 console.log("1" + 2+2);
-//output is 122
+//output is 122 - hence js always works from left to right : remember that
+
+console.log(+true); //-->  1
+console.log(+"");   //-->  0
+
 
 let num1,num2,num3
 num1 = num2 = num3 = 2+2
@@ -59,27 +63,46 @@ console.table([num1,num2,num3]);
 console.log("2">1);
 console.log("02">1);
 
-console.log(null>0) //true
-console.log(null==0) //true
-console.log(null<0)
+console.log(null>0) //false
+console.log(null==0) //false
+console.log(null<=0) //true
+// comparisons convert null to 0 but equality check does not  
 
-console.log(undefined>0);
-console.log(undefined==0);
-console.log(undefined<0);
+console.log(undefined>0); //false
+console.log(undefined==0); //false
+console.log(undefined<0); //true
 
 console.log("2" == 2); //returns true as dataypes are not checked and conversion occurs
-console.log("2" === 2);//false as now datatype is also checked
+console.log("2" === 2);//false as now datatype is also checked -- strict check
 
 
-//Non Primitive or Reference dataype:
+//Non Primitive or Reference (Non Primitive) dataype: based on how the data is kept in memory
+
+//symbols :
+const id = Symbol('123')
+const another_id = Symbol('123')
+
+console.log(id === another_id)  // false
+
+//BIG INT:
+const bignum = 12345678905689n // : bigint
+
 // Arrays, Objects, Function as a variables-func obj
 const arr_heroes = ["shaktiman","balveer","bhim"];
 
+//objects : 
 let myobj = {
     name:"Aditi",
     age:22,
 }
 
+
+//function as a variable: 
 let myfunc = function(){
     console.log("Hi");
 }
+//using type of to get datatype : 
+// null is type of object : 
+// undefined is undefined :  
+// object is function : 
+// function is obj function
