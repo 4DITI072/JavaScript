245036a3ed7ea87e1f1f353311abf1cd1d24d135
Aditi Kaushal <119050868+4DITI072@users.noreply.github.com>
2025-02-12 22:47:14 +0530

    Update test.js

--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,6 @@
 console.log("Aditi")
+/* "use strict" : treat all  JS code as newer version*/
+// alert(3+3) : available but not in node js
+
+console.log(3+3) //: code readability is priortised
+//datatypes and ecma standards : mdn doc
